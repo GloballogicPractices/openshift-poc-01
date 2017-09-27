c5d850ef8d21ef7f2d101c40ae737933a963cb6a
Andriy Gnennyy <andriy.gnennyy@globallogic.com>
2017-09-27 17:48:43 +0300

    Extended host init and cluster up scripts

--- scripts/host/cluster-up.sh
+++ scripts/host/cluster-up.sh
@@ -2,3 +2,7 @@
 ################################################################################
 # Launch OpenShift cluster                                                     #
 ################################################################################
+
+cd /var/origin/bin/
+
+oc cluster up --host-data-dir /var/openshift/data/ --use-existing-config --public-hostname=pocopenshift.northeurope.cloudapp.azure.com
--- scripts/host/configure-host.sh
+++ scripts/host/configure-host.sh
@@ -2,3 +2,57 @@
 ################################################################################
 # Configure host to launch OpenShift cluster                                   #
 ################################################################################
+
+sudo su
+
+# Optional part - mounting 120 GB drive as /var/origin
+
+mkdir /var/origin
+
+parted -s -a optimal /dev/sdc mklabel gpt -- mkpart primary xfs 1 -1
+
+/sbin/mkfs -t xfs -f /dev/sdc1
+
+sudo echo "/dev/sdc1  /var/origin  xfs  defaults 0 0" >> /etc/fstab
+
+mount /dev/sdc1
+
+# Installing pre-requisite software
+
+yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
+
+yum -y update
+
+yum -y install docker-1.12.6
+
+systemctl enable docker
+
+systemctl start docker
+
+sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --log-driver=journald --signature-verification=false --insecure-registry 172.30.0.0/16"' \
+/etc/sysconfig/docker
+
+systemctl stop docker
+
+systemctl start docker
+
+# Download & configure
+
+cd /var/origin
+
+wget https://github.com/openshift/origin/releases/download/v3.6.0/openshift-origin-server-v3.6.0-c4dd4cf-linux-64bit.tar.gz
+
+tar -x -f openshift-origin-server-v3.6.0-c4dd4cf-linux-64bit.tar.gz
+
+rm *.tar.gz
+
+mv openshift-origin-server-v3.6.0-c4dd4cf-linux-64bit bin
+
+export PATH=$(pwd):$PATH
+
+cd /var/origin/
+
+mkdir data
+
+
+
