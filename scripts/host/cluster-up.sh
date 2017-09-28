b1ea0a0aaa76025b010d633221360ba9e0d056fe
Andriy Gnennyy <andriy.gnennyy@globallogic.com>
2017-09-28 10:38:05 +0300

    Script improvements

--- scripts/host/cluster-up.sh
+++ scripts/host/cluster-up.sh
@@ -5,6 +5,4 @@
 
 cd /var/lib/origin/bin/
 
-#oc cluster up --host-data-dir /var/lib/origin/data/ --use-existing-config --public-hostname=pocopenshift.northeurope.cloudapp.azure.com
-
-oc cluster up --host-data-dir /var/lib/origin/data/ --public-hostname=52.138.147.33.nip.io --env=ROUTER_SUBDOMAIN=52.138.147.33.nip.io
+sudo oc cluster up --host-data-dir /var/lib/origin/data/ --public-hostname=52.236.37.25.nip.io --routing-suffix=52.236.37.25.nip.io
